9de6c58633cd6263d52bee6dac5edb3776ccb410
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-29 20:51:20 -0500

    + operator is working, condition incomplete

--- simple_math.py
+++ simple_math.py
@@ -1,3 +1,10 @@
 num1 = float(input("Enter 1st number: "))
-operator = input("Choose the operator: ") #operator variable can accept any value
+operator = input("Choose the operator (+ - * /): ") #operator variable can accept any value
 num2 = float(input("Enter 2st number: "))
+
+if operator == "+":
+    total = num1 + num2
+    print(f"Result= {total}")
+
+else:
+    print(f"{operator} is not a valid operator...")
