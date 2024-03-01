c859391a0cfb724cef13a8d165fce818de8259d6
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-29 21:03:00 -0500

    program is completed

--- simple_math.py
+++ simple_math.py
@@ -1,3 +1,5 @@
+#SIMPLE CALCULATOR 
+
 num1 = float(input("Enter 1st number: "))
 operator = input("Choose the operator (+ - * /): ") #operator variable can accept any value
 num2 = float(input("Enter 2st number: "))
